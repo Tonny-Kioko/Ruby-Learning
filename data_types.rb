05d761e1073cb2255f42b8e2f756286a6628dee6
Tonny-Kioko <cfakiokoanthony@gmail.com>
2024-06-11 15:05:19 +0300

    data types

---
+++ data_types.rb
@@ -0,0 +1,13 @@
+# strings: for plain texts
+name= "Tonny"
+
+#Numbers: whole numbers and floating point  numbers
+age = 24
+gpa = 3.9
+
+#Boolean: true or false data types
+ismale  = true
+istall = false
+
+#Nil: The variable doesnt have a value
+flaws = Nil
