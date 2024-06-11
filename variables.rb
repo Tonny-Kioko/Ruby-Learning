6afda3fbaa119b17a6957b8660599be20281c0a6
Tonny-Kioko <cfakiokoanthony@gmail.com>
2024-06-11 14:57:56 +0300

    variables

---
+++ variables.rb
@@ -0,0 +1,8 @@
+
+username = "Tonny"
+lang = "Python"
+
+puts ("There once was a guy named " + username)
+puts ("He didnt like that he knew only " + lang)
+puts "He really liked the name"
+puts ("He didnt like knowing only " + lang)
