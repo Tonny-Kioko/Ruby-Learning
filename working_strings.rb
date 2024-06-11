80594a90cf364f02d3ec7f89b04867f2e128f635
Tonny-Kioko <cfakiokoanthony@gmail.com>
2024-06-11 15:15:31 +0300

    working  with strings

---
+++ working_strings.rb
@@ -0,0 +1,17 @@
+phrase = "           DevOps Engineer       "
+
+puts "Devops\" Engineer"
+
+puts "Devop\n Engineer"
+
+puts phrase.upcase()
+puts phrase.downcase()
+puts phrase.strip()
+puts phrase.length()
+puts phrase.include? "Engineer"
+
+puts phrase[12,19]
+
+puts phrase.index("D")
+
+puts "Cloud Engineering".upcase()
