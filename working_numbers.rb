bcdce51a0284e5c6bfade1a9ff755cd51f18fd56
Tonny-Kioko <cfakiokoanthony@gmail.com>
2024-06-11 15:25:12 +0300

    working with numbers

---
+++ working_numbers.rb
@@ -0,0 +1,25 @@
+
+puts 9+10
+puts 9-10
+puts 9/10
+puts 9*10
+puts 9**10
+puts 90%8
+
+puts 1.2 + 7
+
+age=20
+
+puts age
+
+#converting numbers to strings
+puts ("I am " +age.to_s)
+
+#Methods
+puts age.abs()
+puts age.round()
+
+puts age.ceil()
+puts age.floor()
+
+puts Math.sqrt(36)
