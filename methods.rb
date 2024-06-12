7b31f0bb03022c71e428d1817701a46fb4b9133e
Tonny-Kioko <cfakiokoanthony@gmail.com>
2024-06-12 10:42:09 +0300

    Working with methods

---
+++ methods.rb
@@ -0,0 +1,7 @@
+# Blocks of code that perform specific tasks, I think its closer to views in django?
+
+def greetings(name = "Anthony", age = 24)
+  puts "Good morrow DevOps Engineer #{name}. Welcome to #{age}"  
+end
+
+greetings
