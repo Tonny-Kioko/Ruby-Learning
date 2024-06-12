e74edb2f33392fa9bbe8b2a709c48b1e640214d1
Tonny-Kioko <cfakiokoanthony@gmail.com>
2024-06-12 10:33:32 +0300

    Working with hashes and key value pairs

---
+++ hashes.rb
@@ -0,0 +1,11 @@
+#Data structure that stores multiple pirces of information with key value pairs
+#Arrays VS Hashes
+
+counties = {
+  :Nairobi => "NBI", 
+  :Mombasa => "MSA", 
+  :Kisumu => "KSM"
+}
+puts counties
+
+puts counties[:Nairobi]
