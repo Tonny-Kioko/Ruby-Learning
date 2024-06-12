96e818d42e4f032917fd97443a86c015c172bf91
Tonny-Kioko <cfakiokoanthony@gmail.com>
2024-06-12 10:14:49 +0300

    Building a mad libs game in ruby

---
+++ mad_libs.rb
@@ -0,0 +1,12 @@
+puts "Enter a color: "
+color = gets.chomp()
+
+puts "Enter a plural noun: "
+noun = gets.chomp()
+
+puts "Enter a Celebrity name: "
+celebrity = gets.chomp()
+
+puts "Roses are #{color}"
+puts "#{noun} are blue"
+puts "I love #{celebrity}"
