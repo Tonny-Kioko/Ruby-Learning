a03763ab1f9721990d25293cec29bd208cc531a0
Tonny-Kioko <cfakiokoanthony@gmail.com>
2024-06-12 12:43:22 +0300

    implementing for loops

---
+++ for_loops.rb
@@ -0,0 +1,15 @@
+# Allows looping and doing something in a collection of things
+
+friends = Array["Matthew", "Mark", "Luke", "John"]
+
+for friend in friends
+  puts friend
+end
+
+for index in 0..10
+  puts index
+end
+
+7.times do |index|
+  puts index
+end
