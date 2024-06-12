f2dfb4301ac026655c167de14f919f6b3ada682f
Tonny-Kioko <cfakiokoanthony@gmail.com>
2024-06-12 10:26:01 +0300

    working with arrays

---
+++ arrays.rb
@@ -0,0 +1,14 @@
+disciples = Array["James: Fisherman", "John: Tax collector", "Matthew: Overall bad Guy"]
+puts disciples[2]
+
+disciples[2] = "Matthew: Changed tax collector"
+puts disciples[2]
+
+
+friends = Array.new
+friends[0] = "John"
+friends[2] = "Andrew"
+friends[1] = "Judas"
+
+puts friends.reverse
+puts friends.sort
